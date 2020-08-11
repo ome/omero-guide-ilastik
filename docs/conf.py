9adf18ddcd15ae9fe3b5b5ca98d6a2c5714143bb
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-08-11 20:30:02 +0100

    change version

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ copyright = u'2019-2020, Open Microscopy Environment'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.0'
+version = '0.2.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
