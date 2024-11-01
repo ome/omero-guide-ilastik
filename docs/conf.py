417b227e53a3351484873d85e7ab9f32b7121b43
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-11-01 14:12:55 +0000

    Change year and version

--- docs/conf.py
+++ docs/conf.py
@@ -50,14 +50,14 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OMERO Ilastik guide'
-copyright = u'2019-2021, Open Microscopy Environment'
+copyright = u'2019-2024, Open Microscopy Environment'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.1'
+version = '0.3.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
